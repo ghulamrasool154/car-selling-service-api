d94eb9d6b3f0ed46cf1552c3019cdc8e084eb51d
Ghulam Rasool <80417750+ghulamrasool154@users.noreply.github.com>
2024-11-28 16:19:31 +0500

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { login } = require("../controllers/usercontrollers");
+const { login, getUsers } = require("../controllers/user.controllers");
 const router = express.Router();
 
-router.route("/").post(login);
+router.route("/").post(login).get(getUsers);
 
 module.exports = router;
